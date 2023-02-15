5cb314fe0d37dfde63c9010ff6ae551c19cb1b38
betiselmejor <VZnu4288>
2023-02-15 03:46:25 +0100

    hola2

--- app/src/main/java/com/example/a3xwars2/lobby.java
+++ app/src/main/java/com/example/a3xwars2/lobby.java
@@ -2,13 +2,41 @@ package com.example.a3xwars2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class lobby extends AppCompatActivity {
+
+    Button addAFriend, battleAFriend, offlineBattle , randomBattle;
+    ImageView message, options, shop, profile;
+    TextView username;
+    String usernameS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
+        usernameS = getIntent().getExtras().getString("userName");
+        initall();
+    }
+
+    public void initall() {
+        addAFriend = (Button) findViewById(R.id.addAFriendbtn);
+        battleAFriend = (Button) findViewById(R.id.BattleAFriendbtn);
+        offlineBattle = (Button) findViewById(R.id.offlineBattlebtn);
+        randomBattle = (Button) findViewById(R.id.randomBattleebtn);
+
+
+        message = (ImageView) findViewById(R.id.messagebtn);
+        options = (ImageView) findViewById(R.id.optionsbtn);
+        shop = (ImageView) findViewById(R.id.shopbtn);
+        profile = (ImageView) findViewById(R.id.profilebtn);
+
+        username = (TextView) findViewById(R.id.usernameLobbyTV);
+        username.setText(usernameS);
+
     }
 }
--- app/src/main/java/com/example/a3xwars2/loginPage.java
+++ app/src/main/java/com/example/a3xwars2/loginPage.java
@@ -2,13 +2,47 @@ package com.example.a3xwars2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class loginPage extends AppCompatActivity {
+
+    EditText usernameET, passwordET;
+    Button login, offline;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        initall();
+    }
+
+    public void initall(){
+        login = findViewById(R.id.loginbtn);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent changeLoginLobby = new Intent(loginPage.this, lobby.class);
+                changeLoginLobby.putExtra("userName", usernameET.getText());
+                startActivity(changeLoginLobby);
+            }
+        });
+
+
+
+        offline = findViewById(R.id.goOflinebtn);
+        offline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        usernameET = findViewById(R.id.usernameET);
+        passwordET = findViewById(R.id.passwordET);
     }
 }
