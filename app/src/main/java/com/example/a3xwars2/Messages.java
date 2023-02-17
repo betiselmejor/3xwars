be708792a65c17a00abac373ec8d1d5351bd1481
aochoa <aochoa21@jaumebalmes.net>
2023-02-17 01:28:09 +0100

    hoka

---
+++ app/src/main/java/com/example/a3xwars2/Messages.java
@@ -0,0 +1,15 @@
+package com.example.a3xwars2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Messages extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messages);
+    }
+
+}
